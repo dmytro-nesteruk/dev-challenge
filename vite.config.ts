87f18b43a1fbc58c214b6e1bf296d10408b36ef8
Dmytro Nesteruk <d.nesteruk@lampalampa.net>
2022-10-28 20:45:29 +0300

    changed out dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/dev-challenge/',
+  build: {
+    outDir: './build',
+  },
   server: {
     https: true,
     host: '0.0.0.0',
     port: 3000,
   },
-  base: '/dev-challenge/',
 });
