148572bb82c851f5683e86e4863bcef2f34d6908
Dmytro Nesteruk <d.nesteruk@lampalampa.net>
2022-10-28 20:16:49 +0300

    asd

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:m31odyWeb/dev-challenge/.git master:gh-pages
+git push -f git@github.com:m31odyweb/dev-challenge/.git master:gh-pages
 
 cd -
